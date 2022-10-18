dc88d084920ea90d4624ac21c3f850622ab9b678
santa0708 <103973950+santa0708@users.noreply.github.com>
2022-10-18 10:12:42 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,9 +1,7 @@
-var who66='world';
-function sayhello66(){alert('Hello,'+who66+'!');}
+var who66 = 'world';
+function sayhello66() {alert('Hello,'+who66+'!');}
+function taro66() {who66='太郎';}
+function taro66() {who66='太郎';alert(who66+'が来た'+'!');}
 
-function taro66(){who66='太郎';}
-
-function cametaro66(){alert('太郎'+'が来た');}
-
-function hanako66(){who66='花子';}
-function camehanako66(){alert('花子' + 'が来た');}
+function hanako66() {who66='花子';}
+function hanako66() {who66='花子';alert(who66+'が来た'+'!');}
