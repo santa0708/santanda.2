d4e9b0af54219fb2b13e5b1b12103c716b3f742d
santa0708 <103973950+santa0708@users.noreply.github.com>
2022-11-08 10:05:01 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,2 +1,2 @@
 function imgchange1()
-{document.getElementById('img1').innerHTML = '<img src="./image/城西大学マスコットキャラクター.png">';}
+{document.getElementById('img1').innerHTML='<img src="./image/城西大学マスコットキャラクター.png">';}
