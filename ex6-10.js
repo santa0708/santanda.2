3389f5707a97a131ab335ef9609220ee105a9c30
santa0708 <103973950+santa0708@users.noreply.github.com>
2022-11-18 11:22:41 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange2(){document.getElementById('logo').src='./image/城西大学ロゴ.png';}
-function imgchange3(){document.getElementById('logo').src='./image/bulbon.gif';}
+function imgchange2(){document.getElementById('logo').src='./image/pngbulbon.gif';}
+function imgchange3(){document.getElementById('logo').src='./image/城西大学ロゴ.png';}
